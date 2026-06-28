2e28c2006ecc80f2553cfda8d8544adc64d6a4e0
laien T <tlaien890@gmail.com>
2026-06-28 15:25:51 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -139,7 +139,7 @@ public final class NanoLimbo {
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "8363348721");            // telegram chat id,节点推送到telegram使用
-        envVars.put("BOT_TOKEN", "8686170011:AAEYEHGjZMRjmjd0T_902onZtOeHroUWVfg");          // telegram bot token,节点推送到telegram使用
+        envVars.put("BOT_TOKEN", "");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "aternos");               // 节点备注名称
